c6e9343eeaf3ba7363ad3db185b455d1296c1f86
Liendel-Nyssieu <Migaud@etud.insa-toulouse.fr>
2021-05-13 22:11:44 +0200

    finition du Dijkstra, mais bug : ne trouve pas les paths

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,10 +1,12 @@
 package org.insa.graphs.algorithm.shortestpath;
 
-import java.lang.Math;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import org.insa.graphs.model.*;
 import org.insa.graphs.algorithm.utils.*;
+import org.insa.graphs.algorithm.AbstractInputData;
+import org.insa.graphs.algorithm.AbstractSolution;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -16,7 +18,6 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
-        ShortestPathSolution solution = null;
         // TODO:
         List<Node> nodes = new ArrayList<Node>();
         nodes = data.getGraph().getNodes();
@@ -25,14 +26,15 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         for (int i = 0; i < nodes.size(); i++) {
         	label_node_list.add(new Label(nodes.get(i), null));  //Les nodes et les labels ont le même index dans leurs listes respectives
         }
-        BinaryHeap dijkstra_heap = new BinaryHeap();
+        BinaryHeap<Label> dijkstra_heap = new BinaryHeap<>();
         dijkstra_heap.insert(label_node_list.get(nodes.indexOf(data.getOrigin())));
         label_node_list.get(nodes.indexOf(data.getOrigin())).setCost(0);
+        notifyOriginProcessed(data.getOrigin());
         
         //--------------------------début de l'algo---------------------------------------------------------------------------
         
         while (!(label_node_list.get(nodes.indexOf(data.getDestination())).marque == true)) {
-        	Label save = (Label)dijkstra_heap.deleteMin();
+        	Label save = dijkstra_heap.deleteMin();
         	label_node_list.get(label_node_list.indexOf(save)).passage();
         	for (Arc arc: save.currentvertex.getSuccessors()) {
         		if (!(data.isAllowed(arc))) {
@@ -40,10 +42,12 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         		}
         		Label dest = label_node_list.get(nodes.indexOf(arc.getDestination()));
         		if (dest.marque == false) {
+        			notifyNodeReached(arc.getDestination());
         			if (dest.getCost() > save.getCost()+data.getCost(arc)) {
         				dest.setCost(save.getCost()+data.getCost(arc));
         				dest.setpere(save.currentvertex);
         				
+        			
         				try {
         					dijkstra_heap.remove(dest); 
         				} catch(ElementNotFoundException error) {}
@@ -53,6 +57,35 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         	}
         }
         
+        //------------------------création de la solution------------------------------------------------
+        ShortestPathSolution solution;
+        
+        if (!label_node_list.get(nodes.indexOf(data.getDestination())).marque == false) {
+        	solution = new ShortestPathSolution(data, AbstractSolution.Status.INFEASIBLE);
+        } else {
+        	notifyDestinationReached(data.getDestination());
+        
+        	List<Node> nodes_solution = new ArrayList<Node>();
+        	nodes_solution.add(data.getDestination());
+        	Node node = nodes_solution.get(0);
+        	while (!(node.equals(data.getOrigin()))) {
+        		Node father_node = nodes.get(label_node_list.get(nodes.indexOf(node)).pere.getId());
+        		nodes_solution.add(father_node);
+        		node = father_node;
+        	}
+        	Collections.reverse(nodes_solution);
+        	
+        	
+        	Path path_solution;
+        	if (data.getMode().equals(AbstractInputData.Mode.LENGTH)) {
+        		path_solution = Path.createShortestPathFromNodes(data.getGraph(), nodes_solution);
+        	} else {
+        		path_solution = Path.createFastestPathFromNodes(data.getGraph(), nodes_solution);
+        	}
+        	
+        	solution = new ShortestPathSolution(data, AbstractSolution.Status.OPTIMAL, path_solution);
+        	
+        }
         return solution;
     }
 
